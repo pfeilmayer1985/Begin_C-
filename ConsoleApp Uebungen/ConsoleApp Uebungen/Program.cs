c943ab01fd3548d5df31dbed8ddb0fd83060ea0d
Pfeilmayer, Robert <Robert.Pfeilmayer@tecalliance.net>
2022-09-15 11:33:05 +0200

    Aufgabe 1 sum of 2 numbers

--- ConsoleApp Uebungen/ConsoleApp Uebungen/Program.cs
+++ ConsoleApp Uebungen/ConsoleApp Uebungen/Program.cs
@@ -11,11 +11,13 @@ namespace ConsoleApp_Uebungen
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Your first number : ");
-            int numA = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your second number : ");
-            int numB = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Sum : {numA+numB}");
+            //Console.WriteLine("Your first number : ");
+            int numA = 5;
+            //int numA = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your second number : ");
+            //int numB = Convert.ToInt32(Console.ReadLine());
+            int numB = 120;
+            Console.WriteLine($"First number is {numA}, second number is {numB}. Sum : {numA+numB}");
             Console.ReadLine();
             
 
