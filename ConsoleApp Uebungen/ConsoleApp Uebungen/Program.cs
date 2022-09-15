68cab0c5b25187f13a0bb210a1cbbd8ef6b7765d
Pfeilmayer, Robert <Robert.Pfeilmayer@tecalliance.net>
2022-09-15 11:37:13 +0200

    Aufgabe 3: Frage den Nutzer nach einem Text und ersetze im Text alle vorkommenden „U“ und „u“ durch „G“ und „g“

--- ConsoleApp Uebungen/ConsoleApp Uebungen/Program.cs
+++ ConsoleApp Uebungen/ConsoleApp Uebungen/Program.cs
@@ -11,17 +11,12 @@ namespace ConsoleApp_Uebungen
         static void Main(string[] args)
         {
 
-            //Console.WriteLine("Your first number : ");
-            int numA = 5;
-            //int numA = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Your second number : ");
-            //int numB = Convert.ToInt32(Console.ReadLine());
-            int numB = 120;
-            Console.WriteLine($"First number is {numA}, second number is {numB}. Sum : {numA+numB}");
-            Console.ReadLine();
-            
+            Console.WriteLine("Your text is :");
+            string userText = Console.ReadLine();
 
-
+            string replacedUserText = userText.Replace('U', 'u').Replace('G', 'g');
+            Console.WriteLine($"Replaced text : {replacedUserText}");
+            Console.ReadLine(); 
 
 
         }
