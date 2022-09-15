c5c8404370674e311820fde0df971c31720991ab
Pfeilmayer, Robert <Robert.Pfeilmayer@tecalliance.net>
2022-09-15 12:01:06 +0200

    Aufgabe 7: Frage den Nutzer nach einer Texteingabe ( es sollen davor und danach Leerzeilen sein) und gebe die Eingabe ohne Leerzeichen wieder aus

--- ConsoleApp Uebungen/ConsoleApp Uebungen/Program.cs
+++ ConsoleApp Uebungen/ConsoleApp Uebungen/Program.cs
@@ -11,11 +11,13 @@ namespace ConsoleApp_Uebungen
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Your text : ");
+            Console.WriteLine("Your text with many spaces for and after please : ");
             string myInput = Console.ReadLine();
 
-            string adThirtyY = myInput.PadRight(30, 'y');
-            Console.WriteLine(adThirtyY);   
+            Console.WriteLine($"Your text initial text is : ={myInput}=");
+            string myAlteredInput = myInput.Trim(' ');
+
+            Console.WriteLine($"Your cleaned up text is : ={myAlteredInput}=");
 
             /* 
              string[] myInputArray = myInput.Split();
