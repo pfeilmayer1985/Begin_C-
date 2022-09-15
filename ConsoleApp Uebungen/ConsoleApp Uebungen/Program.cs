4f6cf32f09eb35ca9be9640ce8c891ca9dbae8b0
Pfeilmayer, Robert <Robert.Pfeilmayer@tecalliance.net>
2022-09-15 11:29:14 +0200

    Ask for 2 numbers and calculate te sum

---
+++ ConsoleApp Uebungen/ConsoleApp Uebungen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Uebungen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Your first number : ");
+            int numA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your second number : ");
+            int numB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Sum : {numA+numB}");
+            Console.ReadLine();
+            
+
+
+
+
+        }
+    }
+}
