ded6393773e189c3cb99dcd619bd91396e6a8d32
Pfeilmayer, Robert <Robert.Pfeilmayer@tecalliance.net>
2022-09-15 09:34:59 +0200

    Ask user for his age and ask how old he will be in X years

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -13,9 +13,12 @@ namespace beginning
             Console.WriteLine("Wie heißt du?");
             string userName = Console.ReadLine();
             Console.WriteLine($"Hello {userName}");
-            Console.WriteLine("Wie alt bist du?");
+            Console.WriteLine("Wie alt bist du aktuell ?");
             int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"{userName}, du bist also {age} ");
+            Console.WriteLine("Wie alt bist du in X Jahren?");
+            int nextYear = Convert.ToInt32(Console.ReadLine());
+            int sumYears = age + nextYear;
+            Console.WriteLine($"{userName}, du bist also in {nextYear} Jahren, {sumYears}");
             Console.ReadLine();
         }
     }
